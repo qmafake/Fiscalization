17f114113bc181cddcb8a3da0992d2a7e1f55cfa
Quincy <blazwablaz@gmail.com>
2025-08-11 12:45:21 +0200

    First commit

---
+++ src/main/java/za/co/tradelink/fiscalization/FiscalizationApplication.java
@@ -0,0 +1,13 @@
+package za.co.tradelink.fiscalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/tradelink/fiscalization/FiscalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tradelink.fiscalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
